19d11ca058b671fd2d4c434e6607a7afa58f5c1f
Jordan Buschman <jordan.buschman@me.com>
2025-06-06 19:15:16 -0300

    Add screen, loading icon

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,6 +3,9 @@ const SITE_KEY = '6LeF_FcrAAAAAKgzqzmP6gykaXlf5Y4Y_LOi9Bq5';
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
+    const screen = document.getElementsByClassName('screen')[0];
+    screen.classList.remove('hidden')
+
     grecaptcha.ready(async () => {
         const token = await grecaptcha.execute(SITE_KEY, { action: 'LOGIN' });
 
@@ -15,6 +18,7 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
             payload[key] = value;
         });
  
+        // TODO: try / catch
         const response = await fetch('https://api.julionunesmd.com/email', {
             method: 'POST',
             headers: {
@@ -23,5 +27,7 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
             },
             body: JSON.stringify(payload)
         });
+
+        screen.classList.add('hidden');
     });
 });
